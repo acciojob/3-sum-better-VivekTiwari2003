3adc6b8749cdf35dc702d83c77391f7f6d0fdbc9
Vivek-Tiwari <127580268+VivekTiwari2003@users.noreply.github.com>
2023-05-11 16:56:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function threeSum(arr, target) {
+// function threeSum(arr, target) {
 // write your code here
-  
+  function threeSum(arr)
+{
+  return arr[0] + arr[1] + arr[2];
 }
+
+// console.log(sum_three([10, 32, 20]));  
+// console.log(sum_three([5, 7, 9])); 
+// console.log(sum_three([0, 8, -11]));
+
 
 module.exports = threeSum;
